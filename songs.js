17cc68bd7cfc5adabaf61c9e661291acdac26093
Meghan Debity <mdebity@gmail.com>
2017-02-20 20:52:01 -0600

    moved

--- songs.js
+++ songs.js
@@ -5,3 +5,16 @@ songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast 
 songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
 songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
 songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
+
+
+// Each student must add one song to the beginning and the end of the array.
+
+//.push("songs and shit")
+// Loop over the array and remove any words or characters that obviously don't belong.
+// Students must find and replace the > character in each item with a - character.
+// Must add each string to the DOM in index.html in the main content area.
+
+
+
+// {Song name} by {Artist} on the album {Album}
+
