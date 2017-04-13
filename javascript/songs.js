7007eaa3391480aa9b741de3b6d331c276dacc0e
Meghan Debity <mdebity@gmail.com>
2017-04-12 19:36:50 -0500

    delete btn FINALLY working

--- javascript/songs.js
+++ javascript/songs.js
@@ -1,54 +1,49 @@
 var musicContainer = document.getElementById("music-info");
 var musicString = "";
-var music;
+var music = [];
 
-function makeDom(xhrData){
+// var deleteBtn = document.getElementsByClassName("deleteBtn");
+// 	deleteBtn.addEventListener("click", function(){
+
+function eventCreatesNewArray(){
+var deleteBtn = document.getElementsByClassName("deleteBtn");
+	 for(var j = 0; j < deleteBtn.length; j++){
+		deleteBtn[j].addEventListener("click", function (e){
+	 	console.log("something", music);
+			var newMusic = music.splice(event.target.id,1);
+			makeDom(music);
+		})
+	}
+};
+
+function makeDom(musicArray){
 	var counter = 0;
-	for(var i=0; i<xhrData.music.length; i++){
+	musicString = "";
+	for(var i=0; i<musicArray.length; i++){
 		counter = counter+1;
-		music = xhrData.music[i];
-
-		musicString +=`<div id="txt` + counter + `"><h3>${music.song}</h3>`;
-		musicString +=`<p>${music.artist} | ${music.album} | ${music.genre}`;
-		musicString +=`<button class="deleteBtn">Delete</button>`;
+		musicString +=`<div id="txt` + counter + `"><h3>${musicArray[i].song}</h3>`;
+		musicString +=`<p>${musicArray[i].artist} | ${musicArray[i].album} | ${musicArray[i].genre}`;
+		musicString +=`<button class="deleteBtn" id="${i}">Delete</button>`;
 		musicString +=`</p></div>`;
 
 	}
-		musicContainer.innerHTML = musicString;
+		musicContainer.innerHTML = musicString;	
+		eventCreatesNewArray();
+};
 
-	// var deleteBtn = document.getElementsByClassName("deleteBtn");
-	// 	 for(var j = 0; j < deleteBtn.length; j++){
-	// 		deleteBtn[j].addEventListener("click", function (){
-	// 	 	console.log(musicString);
-	// 	 	musicString += `<p>I'll be deleted</p>`;
-
-	// 		musicContainer.innerHTML = musicString;
-
-
-	// 			// musicString.value("");
-	// 		});
-	// 	}
-
+function makeArray(xhrData){
+	music = xhrData.music;
+	makeDom(music);
+};
 	
-}
-
-var deleteBtn = document.getElementsByClassName("deleteBtn");
-	deleteBtn.addEventListener("click", function(){
-		musicString = "";
-
-		musicContainer.innerHTML = musicString;
-	});
-
-
 function loadFile(){
 	var data = JSON.parse(this.responseText);
-	makeDom(data);
-
-}
+	makeArray(data);
+};
 
 function failLoading(){
 	alert("There's been a mistake. Abort. Or try again later. Whatever.")
-}
+};
 
 var myRequest = new XMLHttpRequest(); //~new~ is a constructor
 myRequest.addEventListener("load", loadFile);
